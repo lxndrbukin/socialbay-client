6f1ebd761158b19148f105be7daf4cd7ed558af5
Alexander <lxndrbukin@gmail.com>
2024-12-04 22:15:18 +0000

    Adding ErrorMessage helper func with refactor

--- src/components/Auth/Auth.tsx
+++ src/components/Auth/Auth.tsx
@@ -1,14 +1,14 @@
-import './assets/styles.scss';
-import { useState, FormEvent, FocusEvent } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
-import { AppDispatch, handleAuthErrors, RootState, auth } from '../../store';
-import { useLocation } from 'react-router-dom';
-import axios from 'axios';
-import { AuthFormInputProps } from './types';
-import { inputFields } from './assets/formData';
-import AuthForm from './AuthForm';
-import AuthFormInput from './assets/reusable/AuthFormInput';
-import { SessionProps } from '../../store/slices/types';
+import "./assets/styles.scss";
+import { useState, FormEvent, FocusEvent } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { AppDispatch, handleAuthErrors, RootState, auth } from "../../store";
+import { useLocation } from "react-router-dom";
+import { AuthFormInputProps } from "./types";
+import { inputFields } from "./assets/formData";
+import { returnErrorMessage } from "./helpers";
+import AuthForm from "./AuthForm";
+import AuthFormInput from "./assets/reusable/AuthFormInput";
+import { SessionProps } from "../../store/slices/types";
 
 export default function Auth(): JSX.Element {
   const { pathname } = useLocation();
@@ -19,9 +19,9 @@ export default function Auth(): JSX.Element {
   const [formData, setFormData] = useState<{
     [key: string]: string;
   }>({
-    username: '',
-    email: '',
-    password: '',
+    username: "",
+    email: "",
+    password: "",
   });
 
   const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>): void => {
@@ -39,18 +39,7 @@ export default function Auth(): JSX.Element {
 
   const handleInputUnselect = (e: FocusEvent<HTMLInputElement>): void => {
     const { value, name } = e.target;
-    if (!value.length) {
-      dispatch(handleAuthErrors({ [name]: { value, message: 'Required' } }));
-    }
-    if (value.length) {
-      if (value.length < 3) {
-        dispatch(
-          handleAuthErrors({
-            [name]: { value, message: 'Must be at least 3 characters long' },
-          })
-        );
-      }
-    }
+    dispatch(handleAuthErrors(returnErrorMessage(name, value)));
   };
 
   const handleSubmit = async (e: FormEvent<HTMLFormElement>): Promise<void> => {
@@ -82,7 +71,7 @@ export default function Auth(): JSX.Element {
     );
   });
 
-  const userAction = pathname === '/signup' ? 'Sign Up' : 'Login';
+  const userAction = pathname === "/signup" ? "Sign Up" : "Login";
 
   return (
     <AuthForm onSubmit={handleSubmit} action={userAction}>
