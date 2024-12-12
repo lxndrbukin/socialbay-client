9ffba75d167ff6c27a1b987d319facd77e970099
Alexander <alex.bukin@hotmail.com>
2024-12-12 07:40:45 +0000

    Working on Header styling and functionality

--- src/components/Header/HeaderUserMenu.tsx
+++ src/components/Header/HeaderUserMenu.tsx
@@ -1,20 +1,41 @@
+import { useState } from 'react';
+
+type UserMenuLink = {
+  url: string;
+  text: string;
+}
+
+const links: UserMenuLink[] = [
+  { url: '#', text: 'Profile' },
+  { url: '#', text: 'Settings' },
+  { url: '#', text: 'Log Out' },
+]
+
 export default function HeaderUserMenu(): JSX.Element {
+  const [active, setActive] = useState<boolean>(false);
+ 
+  const handleOnClick = (): void => {
+    setActive(!active);
+  }
+
+  const renderedLinks = links.map(({url, text}: UserMenuLink) => <li><a href={url}>{text}</a></li>);
+
+  const renderedDropdown = active && <div className='header_user_menu-dropdown'>
+    <div className='header_user_menu-dropdown_info'>
+      <div className='avatar-placeholder'></div>
+      nickname
+    </div>
+    <ul className='header_user_menu-dropdown_links'>
+      {renderedLinks}
+    </ul>
+  </div>;
+
   return <div className='header_user'>
   <div className='header_user_menu'>
-    <div className='header_user_menu-icon'>
+    <div onClick={handleOnClick} className='header_user_menu-icon'>
       <div className='icon-placeholder'></div>
     </div>
-    <div className="header_user_menu-dropdown">
-      <div className='header_user_menu-dropdown_info'>
-        <div className='avatar-placeholder'></div>
-        nickname
-      </div>
-      <ul className='header_user_menu-dropdown_links'>
-        <li><a href=''>Link 1</a></li>
-        <li><a href=''>Link 2</a></li>
-        <li><a href=''>Link 3</a></li>
-      </ul>
-    </div>
+    {renderedDropdown}
   </div>
 </div>
 }
