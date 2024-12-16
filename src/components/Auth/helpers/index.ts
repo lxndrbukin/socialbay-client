d591edae6681faaa60b25779fd3da80a0583fad9
Alexander <alex.bukin@hotmail.com>
2024-12-16 21:33:03 +0000

    Minor code styling refactor

--- src/components/Auth/helpers/index.ts
+++ src/components/Auth/helpers/index.ts
@@ -1,9 +1,9 @@
 export const returnErrorMessage = (name: string, value: string) => {
   if (!value.length) {
-    return { [name]: { value, message: "Required" } };
+    return { [name]: { value, message: 'Required' } };
   }
   if (value && value.length < 3) {
-    return { [name]: { value, message: "Must be at least 3 characters long" } };
+    return { [name]: { value, message: 'Must be at least 3 characters long' } };
   }
   return {};
 };
