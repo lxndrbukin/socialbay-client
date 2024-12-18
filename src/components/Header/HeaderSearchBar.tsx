59016f1c7b62829407147e315aa132fc264ff9a5
Alexander <alex.bukin@hotmail.com>
2024-12-18 07:41:48 +0000

    Working on Header functionality

--- src/components/Header/HeaderSearchBar.tsx
+++ src/components/Header/HeaderSearchBar.tsx
@@ -1,6 +1,26 @@
+import { ChangeEvent, FormEvent, useState } from 'react';
+
 export default function HeaderSearchBar(): JSX.Element {
-  return <form className='header_search'>
-    <input name='search' placeholder='Search' />
-    <button>OK</button>
-  </form>
+  const [inputValue, setInputValue] = useState<string>('');
+
+  const handleChange = (e: ChangeEvent<HTMLInputElement>): void => {
+    setInputValue(e.target.value);
+  };
+
+  const handleSubmit = (e: FormEvent<HTMLFormElement>): void => {
+    e.preventDefault();
+    console.log(inputValue);
+  };
+
+  return (
+    <form onSubmit={handleSubmit} className='header_search'>
+      <input
+        defaultValue={inputValue}
+        onChange={handleChange}
+        name='search'
+        placeholder='Search'
+      />
+      <button>OK</button>
+    </form>
+  );
 }
--- src/components/Header/HeaderUserMenu.tsx
+++ src/components/Header/HeaderUserMenu.tsx
@@ -1,41 +1,62 @@
-import { useState } from 'react';
+import { useState, useRef, useEffect } from 'react';
 
 type UserMenuLink = {
   url: string;
   text: string;
-}
+};
 
 const links: UserMenuLink[] = [
   { url: '#', text: 'Profile' },
   { url: '#', text: 'Settings' },
   { url: '#', text: 'Log Out' },
-]
+];
 
 export default function HeaderUserMenu(): JSX.Element {
   const [active, setActive] = useState<boolean>(false);
- 
-  const handleOnClick = (): void => {
-    setActive(!active);
-  }
+  const dropdownRef = useRef<HTMLDivElement>(null);
 
-  const renderedLinks = links.map(({url, text}: UserMenuLink) => <li><a href={url}>{text}</a></li>);
+  useEffect(() => {
+    document.addEventListener('click', handleOutsideClick);
+    return (): void => {
+      document.removeEventListener('click', handleOutsideClick);
+    };
+  }, []);
 
-  const renderedDropdown = active && <div className='header_user_menu-dropdown'>
-    <div className='header_user_menu-dropdown_info'>
-      <div className='avatar-placeholder'></div>
-      nickname
+  const handleInsideClick = (): void => {
+    setActive(true);
+  };
+
+  const handleOutsideClick = (e: MouseEvent) => {
+    if (dropdownRef.current && dropdownRef.current.contains(e.target as Node)) {
+      setActive(false);
+      console.log(active);
+    }
+  };
+
+  const renderedLinks = links.map(({ url, text }: UserMenuLink) => (
+    <li key={text}>
+      <a href={url}>{text}</a>
+    </li>
+  ));
+
+  const renderedDropdown = active && (
+    <div ref={dropdownRef} className='header_user_menu-dropdown'>
+      <div className='header_user_menu-dropdown_info'>
+        <div className='avatar-placeholder'></div>
+        nickname
+      </div>
+      <ul className='header_user_menu-dropdown_links'>{renderedLinks}</ul>
     </div>
-    <ul className='header_user_menu-dropdown_links'>
-      {renderedLinks}
-    </ul>
-  </div>;
+  );
 
-  return <div className='header_user'>
-  <div className='header_user_menu'>
-    <div onClick={handleOnClick} className='header_user_menu-icon'>
-      <div className='icon-placeholder'></div>
+  return (
+    <div className='header_user'>
+      <div className='header_user_menu'>
+        <div onClick={handleInsideClick} className='header_user_menu-icon'>
+          <div className='icon-placeholder'></div>
+        </div>
+        {renderedDropdown}
+      </div>
     </div>
-    {renderedDropdown}
-  </div>
-</div>
+  );
 }
